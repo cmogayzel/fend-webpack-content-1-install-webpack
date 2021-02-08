92753b2f752ae8ed0dfe9d5e502c00edc50fddde
Chuck Mogayzel <mogayzelc@gmail.com>
2021-02-07 22:13:44 -0800

    I am getting an error in the html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -3,19 +3,18 @@ const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
 
 module.exports = {
-    entry: './src/client/index.js',
-    module: {
-        rules: [{
-            test: '/\.js$/',
-            exclude: /node-modules/,
-            loader: "babel-loader"
-        }]
+        entry: './src/client/index.js',
+        module: {
+            rules: [{
+                test: '/\.js$/',
+                exclude: /node-modules/,
+                loader: "babel-loader"
+            }]
 
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: "./src/client/views/index.html",
+                filename: "./index.html",
+            })
+        ]
